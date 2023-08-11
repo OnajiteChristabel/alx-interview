ab756585c91a823a1d891bdc70f72a8b6d18e984
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2023-08-11 03:40:13 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,62 +3,51 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const filmEndPoint = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-let characterUrls = [];
-const characterNames = [];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+const names = [];
+let people = [];
 
-const fetchCharacterUrls = () => {
-  return new Promise((resolve, reject) => {
-    request(filmEndPoint, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        reject(err || `Status Code - ${res.statusCode}`);
-      } else {
-        const jsonBody = JSON.parse(body);
-        characterUrls = jsonBody.characters;
-        resolve();
-      }
-    });
-  });
-};
-
-const fetchCharacterNames = () => {
-  if (characterUrls.length > 0) {
-    const fetchPromises = characterUrls.map((url) => {
-      return new Promise((resolve, reject) => {
-        request(url, (err, res, body) => {
-          if (err || res.statusCode !== 200) {
-            reject(err || `Status Code - ${res.statusCode}`);
-          } else {
-            const jsonBody = JSON.parse(body);
-            characterNames.push(jsonBody.name);
-            resolve();
-          }
-        });
-      });
-    });
-    return Promise.all(fetchPromises);
-  } else {
-    console.error('Error: No character URLs found.');
-  }
-};
-
-const printCharacterNames = () => {
-  characterNames.forEach((name, index) => {
-    process.stdout.write(name);
-    if (index < characterNames.length - 1) {
-      process.stdout.write('\n');
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
     }
-  });
+  }));
 };
 
-const fetchAndPrintCharacterNames = async () => {
-  try {
-    await fetchCharacterUrls();
-    await fetchCharacterNames();
-    printCharacterNames();
-  } catch (error) {
-    console.error('Error:', error);
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters');
   }
 };
 
-fetchAndPrintCharacterNames();
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
