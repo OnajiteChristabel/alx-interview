873f3ef4935d4c5faa6a0d1de20c05ce0d3abe38
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2023-09-08 03:13:34 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,56 @@
 #!/usr/bin/python3
+'''Prime Game'''
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num == 2:
-        return True
-    if num % 2 == 0:
-        return False
-    for i in range(3, int(num**0.5) + 1, 2):
-        if num % i == 0:
-            return False
-    return True
+def is_winner(num_rounds, num_list):
+    '''Finds the winner'''
+    winners_count = {'Maria': 0, 'Ben': 0}
 
-def compute_primes(limit):
-    primes = []
-    for i in range(2, limit + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
+    for i in range(num_rounds):
+        round_winner = determine_round_winner(num_list[i], num_rounds)
+        if round_winner is not None:
+            winners_count[round_winner] += 1
 
-def play_round(n, primes):
-    remaining_numbers = set(range(1, n + 1))
-    maria_turn = True
-    
-    while True:
-        valid_move = False
-        for prime in primes:
-            if prime in remaining_numbers:
-                valid_move = True
-                remaining_numbers -= set(range(prime, n + 1, prime))
-                maria_turn = not maria_turn
-                break
-        
-        if not valid_move:
-            break
-    
-    return "Maria" if maria_turn else "Ben"
-
-def isWinner(x, nums):
-    primes = compute_primes(1000)
-    maria_wins = 0
-    ben_wins = 0
-    
-    for n in nums:
-        winner = play_round(n, primes)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-    
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
+    if winners_count['Maria'] > winners_count['Ben']:
+        return 'Maria'
+    elif winners_count['Ben'] > winners_count['Maria']:
+        return 'Ben'
     else:
         return None
+
+def determine_round_winner(n, num_rounds):
+    '''Determine the round winner'''
+    num_range = [i for i in range(1, n + 1)]
+    players = ['Maria', 'Ben']
+
+    for i in range(n):
+        current_player = players[i % 2]
+        selected_indices = []
+        prime = -1
+
+        for idx, num in enumerate(num_range):
+            if prime != -1:
+                if num % prime == 0:
+                    selected_indices.append(idx)
+            else:
+                if is_prime(num):
+                    selected_indices.append(idx)
+                    prime = num
+
+        if prime == -1:
+            if current_player == players[0]:
+                return players[1]
+            else:
+                return players[0]
+        else:
+            for idx, val in enumerate(selected_indices):
+                del num_range[val - idx]
+    return None
+
+def is_prime(n):
+    if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
+        return False
+    else:
+        for i in range(3, int(n**(1/2)) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
